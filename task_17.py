26b5243c11b402bceb9e9dc57b6bd0e094bb7608
Liliya <105841191+Bayliliya@users.noreply.github.com>
2023-03-17 18:47:10 +0300

    Solution task19

--- task_17.py
+++ task_17.py
@@ -1,3 +1,4 @@
+
 #  Дан список чисел. Определите, сколько в нем
 # встречается различных чисел.
 # Input: [1, 1, 2, 0, -1, 3, 4, 4]
@@ -7,5 +8,5 @@ LIST_1 = [2, 4, 8, 8, 3, 5, 2, 4, 3, 3]
 LIST_Modify = []
 for i in LIST_1:
     if i not in LIST_Modify:
-        LIST_Modify.append(i)        
-print(LIST_Modify, len(LIST_Modify), sep="//")
+        LIST_Modify.append(i)
+print(LIST_Modify, len(LIST_Modify), sep='//')
